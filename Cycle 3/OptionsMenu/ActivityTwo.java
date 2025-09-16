37dbe69684961a5c52c26ef68b725ca3cefce27b
BinilBabuGeorge <binilbabugeorge@gmail.com>
2025-09-16 14:27:56 +0530

    Create ActivityTwo.java

---
+++ Cycle 3/OptionsMenu/ActivityTwo.java
@@ -0,0 +1,14 @@
+package com.example.navigateactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activitytwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitytwo);
+    }
+}
